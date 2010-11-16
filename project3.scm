28912dca61a5ff35082562201916cf02877fcd11
Arnór Barkarson <arnorb06@ru.is>
2010-11-16 11:16:23 +0000

    1 c

--- project3.scm
+++ project3.scm
@@ -24,3 +24,17 @@
 
 (duplicate '(1 2 3 4))
 
+
+
+(define (delete list i)
+  (COND ((< (length list) i)
+         (cons (CAR list) (CDR list)))
+        ((equal? 1 i)
+         (CDR list))
+        (else
+         (cons (CAR list)(delete (CDR list)(- i 1))))
+  )
+)
+
+  (delete '(1 2) 3)
+  (delete '(1 2 3 4 5) 3)
